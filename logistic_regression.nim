47c1b907b2d5db359598a672ae69c0f81e96767e
Eduardo Bart <edub4rt@gmail.com>
2017-09-23 16:18:00 -0300

    Fixes and add numpy implement for comparison

--- logistic_regression.nim
+++ logistic_regression.nim
@@ -1,4 +1,4 @@
-import sequtils, times, future
+import sequtils, times, future, random
 import arraymancer, arraymancer_vision
 
 # Utilities
@@ -8,21 +8,23 @@ proc unsafeUnsqueeze*(t: Tensor, axis: int): Tensor =
   t.reshape(shape)
 proc toTensor*[T](s: openarray[Tensor[T]]): Tensor[T] =
   s.map(proc(t: Tensor[T]): Tensor[T] = t.unsafeUnsqueeze(0)).concat(0)
+proc abs*[T](t: Tensor[T]): Tensor[T] =
+  t.map(proc(x: T):T = abs(x))
+
+# Benchmarking timer
 proc timer*(what: string = "") =
   var start {.global.} = cpuTime()
   if what.len > 0:
     let now = cpuTime()
-    echo what, " [", (cpuTime() - start)* 1000, "ms]"
+    echo what, " [", (now - start)* 1000, "ms]"
   start = cpuTime()
-proc abs*[T](t: Tensor[T]): Tensor[T] =
-  t.map(proc(x: T):T = abs(x))
 
 # Load dataset
 proc loadDataset(path: string): auto =
   let
     cats = loadFromDir(path & "/cat")
     noncats = loadFromDir(path & "/noncat")
-    inputs = concat(cats, noncats).toTensor()
+    inputs = concat(cats, noncats).toTensor().astype(float32)
     labels = concat(ones([cats.len], float32), zeros([noncats.len], float32), 0)
   return (inputs, labels)
 
@@ -33,10 +35,13 @@ var (test_dataset_x, test_dataset_y) = loadDataset("data/catvsnoncat/test")
 timer("Loaded data")
 
 # Visualize dataset samples with visdom
-#let vis = newVisdomClient()
-#vis.image(tile_collection(train_dataset_x[0..<16, _, _, _]), "Train Samples")
-#vis.image(tile_collection(test_dataset_x[0..<16, _, _, _]), "Test Samples")
-#timer("Data visualization sent to visdom")
+try:
+  let vis = newVisdomClient()
+  vis.image(tile_collection(train_dataset_x.unsafeSlice(0..<16, _, _, _).astype(uint8)), "Train Samples")
+  vis.image(tile_collection(test_dataset_x.unsafeSlice(0..<16, _, _, _).astype(uint8)), "Test Samples")
+  timer("Data visualization sent to visdom")
+except:
+  echo "Visualization not shown because visdom was not reacheable!"
 
 # Preprocess input data, all pixels for each image are flattened into
 # column vectors, thus different columns stores different examples
@@ -45,47 +50,58 @@ let
   m_train = train_dataset_x.shape[0]
   m_test = test_dataset_x.shape[0]
 var
-  train_x = train_dataset_x.reshape([m_train, num_features]).transpose().astype(float32)
-  train_y = train_dataset_y.reshape([1, m_train])
-  test_x = test_dataset_x.reshape([m_test, num_features]).transpose().astype(float32)
-  test_y = test_dataset_y.reshape([1, m_test])
-timer("Preprocessed data")
+  train_x = train_dataset_x.unsafeReshape([m_train, num_features]).unsafeTranspose()
+  train_y = train_dataset_y.unsafeReshape([1, m_train])
+  test_x = test_dataset_x.unsafeReshape([m_test, num_features]).unsafeTranspose()
+  test_y = test_dataset_y.unsafeReshape([1, m_test])
 
 # Simple pixel normalization
 train_x /= 255.0f
 test_x /= 255.0f
 
+timer("Preprocessed data")
+
 # Sigmoid function
-proc sigmoid*[T: SomeReal](x: T): T =
+proc sigmoid(x: float32): float32 {.noSideEffect.} =
   1.0f/(1.0f + exp(-x))
 makeUniversalLocal(sigmoid)
 
 # Gradient descent optimize
-proc train(w: var Tensor[float32], b: var float32, X, Y: Tensor[float32], max_iterations: int, learning_rate: float32) =
+proc train(w: var Tensor[float32], b: var float32, X, Y: Tensor[float32],
+           max_iterations: int,
+           learning_rate: float32) =
   let m = X.shape[1].float32
-  var cost: float32
 
-  for i in 0..<max_iterations:
-    # Forward propagation
-    let A = sigmoid((w.transpose() * X) + b.bc([w.shape[1], X.shape[1]]))
-    cost = -sum((Y .* ln(A)) + ((1.0f.bc(Y.shape) - Y) .* ln(1.0f.bc(A.shape) - A))) / m
+  for i in 1..max_iterations:
+    const optimized_version = true
+    when optimized_version:
+      # Forward propagation
+      let sb = b
+      var A = (w.unsafeTranspose() * X).map(x => sigmoid(x + sb))
+      proc cross_entropy(a, y: float32): float32 {.noSideEffect.} =
+        (y * ln(a)) + ((1.0f - y) * ln(1.0f - a))
+      let cost = - fold2(A, 0.0f, (r, a, y) => r + cross_entropy(a, y), Y) / m
+    else:
+      # Forward propagation
+      let A = sigmoid((w.unsafeTranspose() * X) .+ b)
+      let cost = - sum((Y .* ln(A)) + ((1.0f .- Y) .* ln(1.0f .- A))) / m
 
     # Backward propagation
     let
-      dw = (X * (A - Y).transpose()) / m
-      db = sum(A - Y) / m
+      difAY = A - Y
+      dw = X * difAY.unsafeTranspose()
+      db = sum(difAY)
 
     # Gradient descent
-    w -= (learning_rate * dw)
-    b -= (learning_rate * db)
+    w -= (learning_rate / m) * dw
+    b -= (learning_rate / m) * db
 
-    if i mod 100 == 0:
+    if i == 1 or i mod 100 == 0:
       echo "Cost after iteration ", i, ": ", cost
 
 # Prediction
 proc predict(w: Tensor[float32], b: float32, X: Tensor[float32]): Tensor[float32] =
-  let m = X.shape[1]
-  let A = sigmoid((w.transpose() * X) + b.bc([w.shape[1], X.shape[1]]))
+  let A = sigmoid((w.unsafeTranspose() * X) .+ b)
   A.map(x => (x >= 0.5).float32)
 
 # Initialize weights with zeros
@@ -116,5 +132,23 @@ let
   test_pred_y = predict(w, b, test_x)
   train_accuracy = score(train_pred_y, train_y)
   test_accuracy = score(test_pred_y, test_y)
+
 echo "Train accuracy: ", train_accuracy, "%"
 echo "Test accuracy: ", test_accuracy, "%"
+
+# Show some test examples
+try:
+  let vis = newVisdomClient()
+  for i in 0..<test_dataset_x.shape[0]:
+    var text: string
+    if test_pred_y[0, i] == 1.0f:
+      text = "is cat"
+    else:
+      text = "non cat"
+    let img = test_dataset_x
+      .unsafeSlice(i, _, _, _)
+      .unsafeReshape(test_dataset_x.channels, test_dataset_x.height, test_dataset_x.width)
+      .astype(uint8)
+    vis.image(img, "test" & $i, text)
+except:
+  discard
---
+++ logistic_regression.py
@@ -0,0 +1,100 @@
+import numpy as np
+import glob
+from PIL import Image
+import time
+
+def timer(s):
+  global start
+  if len(s) > 0:
+    now = time.time()
+    print(s, (now - start) * 1000, "ms")
+  start = time.time()
+
+# Load dataset
+def loadDataset(path):
+  cats = [np.array(Image.open(fname)) for fname in glob.glob(path + '/cat/*')]
+  noncats = [np.array(Image.open(fname)) for fname in glob.glob(path + '/noncat/*')]
+  inputs = np.concatenate((cats, noncats), 0)
+  labels = np.concatenate((np.ones((len(cats))), np.zeros((len(noncats)))), 0)
+  return [inputs, labels]
+
+print("Loading data...")
+timer('')
+train_dataset_x, train_dataset_y = loadDataset("data/catvsnoncat/train")
+test_dataset_x, test_dataset_y = loadDataset("data/catvsnoncat/test")
+timer("Loaded data")
+
+# Preprocess input data, all pixels for each image are flattened into
+# column vectors, thus different columns stores different examples
+num_features = train_dataset_x.shape[1] * train_dataset_x.shape[2] * train_dataset_x.shape[3]
+m_train = train_dataset_x.shape[0]
+m_test = test_dataset_x.shape[0]
+train_x = train_dataset_x.reshape((m_train, num_features)).transpose().astype(np.float32)
+train_y = train_dataset_y.reshape((1, m_train))
+test_x = test_dataset_x.reshape((m_test, num_features)).transpose().astype(np.float32)
+test_y = test_dataset_y.reshape((1, m_test))
+
+# Simple pixel normalization
+train_x /= 255.0
+test_x /= 255.0
+
+timer("Preprocessed data")
+
+# Sigmoid function
+def sigmoid(x):
+  return 1.0/(1.0 + np.exp(-x))
+
+# Gradient descent optimize
+def train(w, b, X, Y, max_iterations, learning_rate):
+  m = X.shape[1]
+  for i in range(1,max_iterations+1):
+    # Forward propagation
+    A = sigmoid(np.dot(w.transpose(), X) + b)
+    cost = - np.sum((Y * np.log(A)) + ((1.0 - Y) * np.log(1.0 - A))) / m
+
+    # Backward propagation
+    difAY = A - Y
+    dw = np.dot(X, difAY.transpose())
+    db = np.sum(difAY)
+
+    # Gradient descent
+    w -= (learning_rate / m) * dw
+    b -= (learning_rate / m) * db
+
+    if i == 1 or i % 100 == 0:
+      print("Cost after iteration ", i, ": ", cost)
+
+# Prediction
+def predict(w, b, X):
+  A = sigmoid(np.dot(w.transpose(), X) + b)
+  return np.where(A <= 0.5, 0, 1)
+
+# Initialize weights with zeros
+w = np.zeros((num_features, 1))
+b = 0.0
+
+# Print some information
+print("Training inputs shape: ", train_dataset_x.shape)
+print("Training labels shape: ", train_dataset_y.shape)
+print("Test inputs shape: ", test_dataset_x.shape)
+print("Test labels shape: ", test_dataset_y.shape)
+print("Total number of parameters: ", num_features + 1)
+
+# Train on data
+print("Training...")
+train(w, b, train_x, train_y,
+      max_iterations=2000,
+      learning_rate=5e-3)
+timer("Train finished!")
+
+# Show accuracy
+def score(pred_y, y):
+  return (1.0 - np.mean(np.abs(pred_y - y))) * 100.0
+
+train_pred_y = predict(w, b, train_x)
+test_pred_y = predict(w, b, test_x)
+train_accuracy = score(train_pred_y, train_y)
+test_accuracy = score(test_pred_y, test_y)
+
+print("Train accuracy: ", train_accuracy, "%")
+print("Test accuracy: ", test_accuracy, "%")
